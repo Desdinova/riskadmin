15a9e0d71468c60d3d7f1b601bbbaefc36e2d5bf
Desdinova <florian@infra.net>
2018-02-01 17:01:07 +0100

    cli options for -h and -l implemented

--- riskadmin.py
+++ riskadmin.py
@@ -2,22 +2,38 @@
 #
 
 import mysql.connector
-import sys
+import sys			# to get command line arguments via argv
+import getopt			# to parse command line arguments
+
+helptext = "This text should help you do your job"
+
+def cli(argv):
+  try:
+    options, arguments = getopt.getopt(argv,"hl")
+  except getopt.GetoptError:
+    print("type riskadmin -h for help")
+    sys.exit(2)
+  for opt, arg in options:
+    if opt == "-h":
+      print(helptext)
+    elif opt == "-l":
+      conn = mysql.connector.connect(user="root", database="asterisk")
+      cursor = conn.cursor()
+      query = ("select * from user")
+      cursor.execute(query)
+      for id in cursor:
+        print(id)
+      cursor.close()
+      conn.close()
 
 
+if __name__ == "__main__":
+   cli(sys.argv[1:])
 
 
+# print("=======")
 
-conn = mysql.connector.connect(user="root", database="asterisk")
-cursor = conn.cursor()
+# argv contains all arguments passed via command line
+# print(sys.argv)
 
-query = ("select * from user")
-
-cursor.execute(query)
-
-for id in cursor:
-  print(id)
-  
-cursor.close()
-conn.close()
 
